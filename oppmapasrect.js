5e9c29f5f08ef1600f09bffe4ed7fd80d6b125d5
Annelie <annelieberner@gmail.com>
2013-07-31 18:51:57 -0700

    circles according to data

--- oppmapasrect.js
+++ oppmapasrect.js
@@ -7,12 +7,13 @@ d3.json("datasource.json", function(data) {
 
 console.log(data.links)
 var links = data.links;
+var nodes = data.nodes;
 // var width = $('#omap').width();
 // var height = $('#omap').height();
 
 var width = 1600; //for now
 var height = 500; //for now
-
+var strokey = 1;
 var maxResponse = d3.max(data.nodes, function(d,i) { return i;} );
 console.log(maxResponse);
 
@@ -20,9 +21,9 @@ var maxScore = d3.max(data.nodes, function(d) { return d.group;} );
 console.log(maxScore);
 
 var maxTags = d3.max(data.nodes, function(d) { return d.tags;} );
-console.log(maxTags.length);
 console.log(maxTags);
 console.log("maxTags");
+console.log(maxTags.length);
 
 var heightScale = d3.scale.linear()
   .domain([0, maxScore]) //maxScore
@@ -42,7 +43,26 @@ var alongHeight = d3.scale.linear()
 
 var tagWeight = d3.scale.linear()
   .domain([0, maxTags])
-  .range([.5, 1]);
+  .range([20, 100]);
+
+function toTagStroke(id) {
+  // var thisisit = id;
+      // console.log("hey")
+  return (function(d){ 
+    if (d.nodes.id==id){
+      console.log("hey")
+    console.log(d.nodes.tags);
+  }
+  })
+  // var thingie = d3.selectAll("circle")
+    // .data(function(d){ return data.nodes })
+    // .enter().append("circle")
+    // .attr("stroke-width", function(d,i){
+    //   if (id==d.id){
+    //     console.log("hey");
+    //   }
+    // })
+}
 
 var svg = d3.select("#omap").append("svg")
   .attr({
@@ -60,21 +80,37 @@ var rect = svg.selectAll("rect")
     // console.log(d3.ascending(d.group, d.group));
   })
  .enter().append("rect")
- .attr("opacity", function(d,i){
-  if (d.tags!=null){
-    return tagWeight(d.tags.length);
-  }
-  else {
-    return .1;
-  }
- })
+ .attr("opacity", ".2")
+ //  function(d,i){
+ //  if (d.tags!=null){
+ //    return tagWeight(d.tags.length);
+ //  }
+ //  else {
+ //    return .1;
+ //  }
+ // })
  .attr("x", function(d,i){
     return alongWidth(i);
     })
  .attr("y", height/2)
- .attr("width", 6)
- .attr("height", 6)
- .attr("fill", "blue");
+ .attr("width", function(d,i){
+  if (d.tags!=null){
+    return tagWeight(d.tags.length);
+  }
+  else {
+    return 6;
+  }
+ })
+ .attr("height", function(d,i){
+  if (d.tags!=null){
+    console.log(d.tags)
+    return tagWeight(d.tags.length);
+  }
+  else {
+    return 6;
+  }
+ })
+.attr("fill", "blue");
 
 //each piece of data has an ID
 //each ID has a source and a target
@@ -82,13 +118,196 @@ var rect = svg.selectAll("rect")
 //target is to other's ID
 //here is the most basic version of connections between connections! 
 //if you ignore the fact that it's lines not arcs, it works properly in that it links the data correctly
+
+
+var circle = svg.selectAll("circle")
+  .data(function(d){
+    return data.links;
+  })
+  .enter().append("circle")
+  .attr("class", "circ")
+  .attr("opacity", .2)
+  .attr("cx", function(d,i){
+    var thingmid = (alongWidth(d.source)-alongWidth(d.target));
+    if (thingmid>0){
+    return alongWidth(d.target)+(thingmid/2);
+    }
+    else {
+      var posmid = thingmid*(-1);
+      // console.log(posmid);
+      // return posmid+(posmid/2);
+      // return alongWidth(d.target)+(posmid/2);
+      return alongWidth(d.target)-(posmid/2);
+    }
+  })
+  .attr("cy", height/2)
+  .attr("r", function(d,i){
+    var thingmid = (alongWidth(d.source)-alongWidth(d.target));
+    if (thingmid>0){
+    return thingmid/2;
+  }
+  else{
+      var posmid = thingmid*(-1);
+      // console.log(posmid);
+      return posmid/2;
+    }
+  })
+  .attr("fill", "none")
+  .attr("stroke",function(d,i){
+// console.log(strokey)
+    var thingmid = (alongWidth(d.source)-alongWidth(d.target));
+    if (thingmid>0){
+      return "red";
+    } 
+    else {
+      return "blue";
+    }
+  })
+  
+  .attr("stroke-width", function(d,i){
+    var thisis = goBack(i);
+    return thisis;
+  });
+// circle.exit();
+
+// svg.enter().append("circle")
+//   .data(data.nodes)
+//   .attr("stroke-width", function(d,i){
+//     // var thisis = goBack()
+//     // console.log(thisis)
+//     // return thisis;
+//       if (data.nodes.tags!=null){
+//     return tagWeight(data.nodes.tags.length);
+//   }
+//   else {
+//     return .5;
+//   }
+
+//   });
+// circle.exit();
+
+
+function goBack(i){
+// svg.selectAll("circ")
+console.log(data.nodes)
+var other = data.nodes;
+// circ
+//   .data(data.nodes)
+//   .enter()
+  // .append("ul")
+  // function(d){
+  if (other.tags!=null){
+    return tagWeight(other.tags.length);
+  }
+  else {
+    return .5;
+  }
+}
+
+  // .attr("class", "circ")
+  // .transition()
+//   .attr("stroke-width", function(d){ 
+//     if (d.tags!=null){
+//       console.log("hey")
+//       console.log(tagWeight(d.tags.length))
+//       var thisis = tagWeight(d.tags.length)
+//       return thisis;
+//     }
+//     else {
+//       return .5;
+//     }
+//   })
+// }
+
+
+//    .attr("cx", function(d,i){
+//     var thingmid = (alongWidth(d.source)-alongWidth(d.target));
+//     if (thingmid>0){
+//     return alongWidth(d.target)+(thingmid/2);
+//     }
+//     else {
+//       var posmid = thingmid*(-1);
+//       // console.log(posmid);
+//       // return posmid+(posmid/2);
+//       // return alongWidth(d.target)+(posmid/2);
+//       return alongWidth(d.target)-(posmid/2);
+//     }
+//   })
+//   .attr("cy", height/2)
+//   .attr("r", function(d,i){
+//     var thingmid = (alongWidth(d.source)-alongWidth(d.target));
+//     if (thingmid>0){
+//     return thingmid/2;
+//   }
+//   else{
+//       var posmid = thingmid*(-1);
+//       // console.log(posmid);
+//       return posmid/2;
+//     }
+//   })
+//   .attr("fill", "none")
+//   .attr("stroke",function(d,i){
+// // console.log(strokey)
+//     var thingmid = (alongWidth(d.source)-alongWidth(d.target));
+//     if (thingmid>0){
+//       return "red";
+//     } 
+//     else {
+//       return "blue";
+//     }
+//   });
+
+
+// svg.selectAll("circ")
+//   .data(data.nodes)
+//   .enter().append("circle")
+//   .attr("class", "circ")
+//   // .transition()
+//   .attr("stroke-width", function(d){ 
+//     if (d.tags!=null){
+//       console.log("hey")
+//       console.log(tagWeight(d.tags.length))
+//       return tagWeight(d.tags.length);
+//     }
+//     else {
+//       return .5;
+//     }
+//   });
+
+
+
+
+
+
+//access different set of data to give proper stroke weights
+// circle
+//   .data(function(d){ 
+//     return data.nodes;
+//     // console.log(d3.ascending(d.group, d.group));
+//   })
+//  .enter().append("circle")
+//  .transition()
+//  .attr("opacity", function(d,i){
+//   if (d.tags!=null){
+//     console.log("hey")
+//     return tagWeight(d.tags.length);
+//   }
+//   else {
+//     return .1;
+//   }
+//  });
+
+
+
+
+
 var line = svg.selectAll("line")
   .data(function(d){ 
     return data.links;
     // console.log(d3.ascending(d.group, d.group));
   })
  .enter().append("line")
- .attr("opacity", 1)
+ .attr("opacity", .2)
   .attr("x1", function(d,i){   // if (d.source==2 && d.target==0){
   // if (d.target==0){
     // console.log(d.source);
@@ -108,14 +327,93 @@ var line = svg.selectAll("line")
   // }
  })
  .attr("y2", height/2)
- .attr("stroke", "pink")
- .attr("stroke-width", 3);
- // .attr("fill", "pink");
+  .attr("stroke",function(d,i){
+    var thingmid = (alongWidth(d.source)-alongWidth(d.target));
+    if (thingmid>0){
+      return "red";
+    } 
+    else {
+      return "blue";
+    }
+  })
+ .attr("stroke-width", 3)
+ .attr("fill", "pink");
 
 
 
+
+
+
+
+
+
+
+
+//just does a horizontal force layout
+// var force = d3.layout.force()
+//     .charge(-60)
+//     .linkDistance(60)
+//     .size([width, height]);
+
+// force
+//     .gravity(0)
+//     .nodes(data.nodes)
+//     .links(data.links)
+//     .start();
+
+// var line = svg.selectAll("line")
+//       .data(data.links)
+//       .enter().append("svg:line")
+//       .attr("opacity", ".3")
+//       .attr("stroke", "grey")
+//       .attr("stroke-width",3)
+//       .attr("fill","grey")
+//       .attr("x1", function(d, i) { return alongWidth(i); }) //d.source.x
+//       .attr("y1", function(d) { return height/2; }) //d.source.y
+//       .attr("x2", function(d) { return (d.target.x); }) //d.target.x
+//       .attr("y2", function(d) { return height/2; }); //d.target.y
+
+// var node = svg.selectAll(".node")
+//     .data(data.links)
+//     .enter().append("circle")
+//     .attr("class", "node")
+//     .attr("r", 10)
+//     .attr("opacity", ".3")
+//     .attr("fill", "blue")
+//     //   function(d) {
+//     //   if(d.group == 1) {
+//     //     return "blue";
+//     //   } 
+//     //   if(d.group == 2) {
+//     //     return "green";
+//     //   }
+//     //   else {
+//     //     return "pink"; 
+//     //   }
+//     // })
+//     .call(force.drag)
+
+//   force.on("tick", function(e) {
+//     line.attr("x1", function(d, i) { return alongWidth(i); }) //d.source.x;
+//         .attr("y1", function(d) { return height/2 }) //d.source.y;
+//         .attr("x2", function(d) { return (d.target.x); }) //d.target.x;
+//         .attr("y2", function(d) { return height/2 }); //d.target.y;
+
+//       node.attr("cx", function(d,i) { return alongWidth(i); }) // alongWidth(i)
+//         .attr("cy", function(d) { return height/2; }); //not d.y
+//   });
 
 //here are a series of attempts at drawing arcs etc. properly
+// var lineFunction = d3.svg.line()
+//   .x(function(d){ return alongWidth(d.source) })
+//   .y (function(d){ return height/2 })
+//   .interpolate("basis");
+
+// var lineGraph = svg.append("path")
+//   .attr("d", lineFunction(data.links))
+//   .attr("stroke","blue")
+//   .attr("stroke-width", 2)
+//   .attr("fill","none");
 
 //an attempt at drawing paths based on data - doesn't work
 // var linet = d3.svg.line()
